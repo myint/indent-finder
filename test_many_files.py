105755c27ccb39165bdba2aa46331d25fdb441c5
Bluebird <phil at freehackers dot org>
2008-09-06 18:26:03 +0200

    - test vim output along with regular output

--- test_many_files.py
+++ test_many_files.py
@@ -8,7 +8,7 @@
 #
 
 
-from indent_finder import *
+import indent_finder
 
 import os, glob
 import unittest 
@@ -16,11 +16,13 @@ from pprint import pprint
 
 class Test_many_files( unittest.TestCase ):
 
-    def check_file( self, fname, result ):
-        ifi = IndentFinder()
+    def check_file( self, fname, result, expected_vim_result ):
+        ifi = indent_finder.IndentFinder()
+        indent_finder.DEFAULT_TAB_WIDTH = 13
         ifi.parse_file( fname )
         res = str(ifi)
         self.assertEquals( res, result )
+        self.assertEquals( expected_vim_result, ifi.vim_output() )
 
     def test_file_space4( self ):
         l = []
@@ -28,14 +30,16 @@ class Test_many_files( unittest.TestCase ):
         l += glob.glob( 'test_files/space4/*.java' )
         for f in l:
             print 'checking: ', f
-            self.check_file( f , 'space 4' )
+            self.check_file( f , 'space 4', 
+                'set sts=4 | set tabstop=4 | set expandtab | set shiftwidth=4' )
 
     def test_file_space2( self ):
         l = []
         l += glob.glob( 'test_files/space2/*.cpp' )
         for f in l:
             print 'checking: ', f
-            self.check_file( f , 'space 2' )
+            self.check_file( f , 'space 2', 
+                'set sts=2 | set tabstop=2 | set expandtab | set shiftwidth=2' )
 
     def test_file_tab( self ):
         l = []
@@ -44,14 +48,17 @@ class Test_many_files( unittest.TestCase ):
         l += glob.glob( 'test_files/tab/*.py' )
         for f in l:
             print 'checking: ', f
-            self.check_file( f , 'tab %d' % DEFAULT_TAB_WIDTH )
+            self.check_file( f , 'tab %d' % indent_finder.DEFAULT_TAB_WIDTH,
+                'set sts=0 | set tabstop=%d | set noexpandtab | set shiftwidth=%d' %
+                    (indent_finder.DEFAULT_TAB_WIDTH, indent_finder.DEFAULT_TAB_WIDTH) )
 
     def test_file_mixed4( self ):
         l = []
         l += glob.glob( 'test_files/mixed4/*.c' )
         for f in l:
             print 'checking: ', f
-            self.check_file( f, 'mixed tab 8 space 4' )
+            self.check_file( f, 'mixed tab 8 space 4',
+                'set sts=4 | set tabstop=8 | set noexpandtab | set shiftwidth=4' )
         
 
 if __name__ == "__main__":
