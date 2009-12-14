2854d2d9081fdfa60bfffb491be23c7f41899266
Bluebird <phil at freehackers dot org>
2009-12-14 15:42:55 +0100

    - one file to launch the test suite

---
+++ run_tests.py
@@ -0,0 +1,11 @@
+
+
+import unittest
+from test_indent_finder import *
+from test_many_files import *
+
+def main():
+    unittest.main( testRunner = unittest.TextTestRunner( verbosity = 2 ) )
+
+if __name__ == "__main__":
+    main()
