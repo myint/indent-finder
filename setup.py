fd0cd9880a72ad1af4ae2ba840fd3847207e8202
Steven Myint <git@stevenmyint.com>
2013-02-09 11:01:29 -0800

    Format

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ from distutils.core import setup
 import indent_finder
 
 
-setup(name="Indentation Finder",
+setup(name='Indentation Finder',
       version=indent_finder.VERSION,
-      description="Finds of check the indentation used in programming source files",
-      author="Philippe Fremy",
-      author_email="phil at freehackers dot org",
-      maintainer="Philippe Fremy",
-      maintainer_email="phil at freehackers dot org",
-      license="BSD license",
-      url="http://www.freehackers.org/Indent_Finder",
+      description='Finds of check the indentation used in programming source '
+                  'files',
+      author='Philippe Fremy',
+      author_email='phil at freehackers dot org',
+      maintainer='Philippe Fremy',
+      maintainer_email='phil at freehackers dot org',
+      license='BSD license',
+      url='http://www.freehackers.org/Indent_Finder',
       classifiers=['Intended Audience :: Developers',
                    'Environment :: Console',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3'],
-      py_modules=["indent_finder"])
+      py_modules=['indent_finder'])
