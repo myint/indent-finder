778f4e8ba47f8380d930e817341982df18cd64a5
Steven Myint <git@stevenmyint.com>
2013-06-09 13:15:11 -0700

    Document fallback

--- plugin/indent_finder.py
+++ plugin/indent_finder.py
@@ -39,6 +39,8 @@ DEFAULT_RESULT = ('space', 4)
 INDENT_RE = re.compile('^([ \t]+)([^ \t]+)')
 MIXED_RE = re.compile('^(\t+)( +)$')
 
+# Optionally used to fall back to default if pre-indentation line is not found.
+# This is not used by the main line detection algorithm.
 LANGUAGE_PRE_INDENTATION = {
     '.py': ':',
     '.h': '{',
